6a8dfc635931bffd1941213cbf5cf61acf2e47d8
John Katieno <katienoj@gmail.com>
2021-05-06 07:26:39 +0300

    Refresher Routing

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 
 //Setting our port
 const port = 3000;
-app.get('/', (req, res) => res.send('Welcome to the Demo App')) ;
+app.get('/', (req, res) => res.send('Welcome to the Demo App'));
 
 //Defining Route
-app.get('/tproute', function (req, res){
+app.get('/tproute', function (req, res) {
     res.send('This is routing for application developed using Node and Express');
 });
 //Startup our application at http://localhost:3000
