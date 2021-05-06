1ca79c94c8c20effbbe60be945412ac7e9d4f242
John Katieno <katienoj@gmail.com>
2021-05-06 08:54:59 +0300

    Refresher: Created DB and added necessary functions

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
 //modules =======================
 const express = require('express');
 const app = express();
+var mongoose = require('mongoose');
 
 //Setting our port
 const port = 3000;
 app.get('/', (req, res) => res.send('Welcome to the Demo App'));
 
-//Defining Route
+//Configuration==================
+
+//Config Files
+
+var db = require('./config/db');
+console.log("connecting--", db);
+mongoose.connect(db.url); //Mongoose connection created
+
+//Frontend Routes================
+app.get('/', (req, res)=>res.send('Welcome to the Mean Demo App!'));
+
+//Defining the Routes
+
 app.get('/tproute', function (req, res) {
     res.send('This is routing for application developed using Node and Express');
+});
+
+//Sample API route
+//Grab the student model we just created
+var Student = require('./app/models/students');
+app.get('/api/students', function(req, res){
+    //Use Mongoose to get all students in the database
+    Student.find(function(err, students){
+        //If there's an error retrieving, send the error 
+        //Nothing after res.send(err) will execute
+
+        if(err)
+        res.send(err);
+        res.json(students); //Return all students in JSON format
+    });
 });
 //Startup our application at http://localhost:3000
 app.listen(port, () => console.log('Example App listening on port ${port}!'));
